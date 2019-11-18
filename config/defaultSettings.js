84b3216b0c9c352981423b5c692ab6a595943978
aekoo <717750121@qq.com>
2019-11-18 17:59:10 +0800

    first commit

---
+++ config/defaultSettings.js
@@ -0,0 +1,17 @@
+export default {
+  // navTheme: 'dark',
+  navTheme: 'light',
+  primaryColor: '#1890FF',
+  layout: 'sidemenu',
+  contentWidth: 'Fluid',
+  fixedHeader: false,
+  autoHideHeader: false,
+  fixSiderbar: false,
+  colorWeak: false,
+  menu: {
+    locale: true,
+  },
+  title: '有宠在家管理平台',
+  pwa: false,
+  iconfontUrl: '',
+};
