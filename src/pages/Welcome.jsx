5d7ef8e8b532a218d6080b5fe1a85dd1b5ab3123
aekoo <717750121@qq.com>
2019-11-26 14:59:13 +0800

    welcome

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,6 +3,6 @@ import { PageHeaderWrapper } from '@ant-design/pro-layout';
 
 export default () => (
   <PageHeaderWrapper>
-    <iframe src="https://petinhome.net/static/index.html" frameBorder="0" style={{ width: '100%', height: '80vh' }}></iframe>
+    <iframe src="https://petinhome.net/static/time/index.html" frameBorder="0" style={{ width: '100%', height: '80vh' }}></iframe>
   </PageHeaderWrapper>
 );
